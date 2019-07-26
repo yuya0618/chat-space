a52a70010e29381c466ea81aff0a587ddf023e45
yuya0618 <yuya.aki.0618@gmail.com>
2019-07-26 12:39:23 +0900

    settingAjax

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
     config.generators do |g|
+      config.time_zone = 'Tokyo'
+      config.active_record.default_timezone = :local
+
       g.stylesheets false
       g.javascripts false
       g.helper false
