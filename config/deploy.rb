a2c75f99bb0d6374185db597270d469477266dd2
yuya0618 <yuya.aki.0618@gmail.com>
2019-07-30 19:41:09 +0900

    Update.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '(~> 3.1, >= 3.1.0)'
+lock "~> 3.11.0"
 
 set :application, "chat-space"
 set :repo_url, 'git@github.com:yuya0618/chat-space.git'
