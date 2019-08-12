6d2ec7399d8d202c1898e62daaf65a1e63a4cd42
yuya0618 <yuya.aki.0618@gmail.com>
2019-08-12 12:00:00 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,6 @@ server '3.13.16.33', user: 'ec2-user', roles: %w{app db web}
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
@@ -17,7 +16,6 @@ server '3.13.16.33', user: 'ec2-user', roles: %w{app db web}
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
 
 
 # Configuration
