9a9f12fe44dd53abaa8c438515b1de8c529d0bdd
yuya0618 <yuya.aki.0618@gmail.com>
2019-10-17 15:41:08 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,7 +26,6 @@ server '3.13.16.33', user: 'ec2-user', roles: %w{app db web}
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
 
-
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
